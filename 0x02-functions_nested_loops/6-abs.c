7d0adccc26e8977163794cbd3404e416e8f90b77
TRUHUB1 <oladosurukayat@gmail.com>
2022-09-13 22:51:18 +0100

    sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * abs - function that computesabsolute power of integer
+ * @c: int used for function argument
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0);
+	{
+	return (c);
+	}
+	else
+	return (c * -1);
+}
