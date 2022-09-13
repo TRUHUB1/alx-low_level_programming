8e093e12f9d0d2435d2819a8e237dbbf44ea935c
TRUHUB1 <oladosurukayat@gmail.com>
2022-09-13 21:41:02 +0100

    print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,14 @@
 
 /**
  * print_alphabet_x10 - print a b c 10 times
- *
- * Return: Always 0.
  */
+
 void print_alphabet_x10(void)
 {
-	char c, i;
+	int c;
+       	int i;
 
-	for (i = 0; i < 10; c++)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
