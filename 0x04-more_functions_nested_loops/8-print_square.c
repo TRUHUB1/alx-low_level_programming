20fc5ec2038024f18bf98862311ea333d3eae62a
TRUHUB1 <oladosurukayat@gmail.com>
2022-09-15 16:26:05 +0100

    print_square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,7 +15,7 @@ void print_square(int size)
 	{
 		for (inc1 = 0; inc1 < size; inc++)
 		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			for (inc2 = 0; inc2 < size ; inc2++)
 			{
 				putchar('#');
 			}
