4c9af903e1c9beeb5742109e4e9b893cfc4e7810
TRUHUB1 <oladosurukayat@gmail.com>
2022-09-15 13:17:36 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * _isdigit - checks if a chracter is a digit or not
+ * @c: tested chracter
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
+
